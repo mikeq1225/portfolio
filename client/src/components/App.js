4633c922a3734e2d301ef78a796f346b99beb1f0
Michael Quarne <mikedq1225@gmail.com>
2020-06-27 13:40:56 -0700

    nav and showcase done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,17 @@
 import React, { Suspense } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from "./Header"
+import NavBar from "./NavBar"
+import About from "./About"
 
 export default (props) => {
 	return (
 		<Router>
 			<div>
 				<Suspense fallback={<div>Loading...</div>}>
-					<Route exact path="/" component={Header}></Route>
-					<h1>Hello World</h1>
-					<button>GET</button>
-					<a href="#0">Click Me</a>
+					<Route path="/" component={NavBar}></Route>
+					<Route path="/" component={Header}></Route>
+					<Route path="/" component={About}></Route>
 				</Suspense>
 			</div>
 		</Router>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,54 +1,26 @@
 import React from "react"
-import { NavLink, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "../styles/Header.scss"
+import "../typewriter"
 
 export default (props) => {
 	return (
-		<div>
-			<header id="headerHome">
+		<div id="showcase">
+			<header>
 				<div className="container">
-					<nav>
-						<img
-							id="logo"
-							src="/assets/MichaelQuarneLogo.png"
-							alt="Michael Quarne logo"
-						/>
-						<ul>
-							<li>
-								<NavLink to="/" activeClassName="selected">
-									Home
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="/About" activeClassName="selected">
-									About Me
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="/Projects" activeClassName="selected">
-									Projects
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="/Resume" activeClassName="selected">
-									Resume
-								</NavLink>
-							</li>
-						</ul>
-					</nav>
 					<div className="headerContent">
 						<h1>
-							I am Michael the{" "}
+							I am Michael, The{" "}
 							<span
 								className="txtType"
-								data-wait="3000"
-								data-words='["UI/UX Enthusiast", "Application Developer"]'
+								datawait="3000"
+								datawords='["UI/UX Enthusiast", "Application Developer"]'
 							></span>
 						</h1>
 						<p className="lead">
 							I specialize in web applications with a focus on UX/UI Design
 						</p>
-						<Link to="/Projects" className="btnLight">
+						<Link to="/Projects" className="buttonDark">
 							View My Work
 						</Link>
 					</div>
