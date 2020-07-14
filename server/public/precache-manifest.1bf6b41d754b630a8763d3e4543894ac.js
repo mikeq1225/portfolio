aad9b42b5dddfd762f5c3a4b2272aea63d6f34ee
Michael Quarne <mikedq1225@gmail.com>
2020-07-14 11:11:44 -0700

    first build

---
+++ server/public/precache-manifest.1bf6b41d754b630a8763d3e4543894ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdad123e490f77fc9d6e85e71da9245c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de59c745d6f87175dcba",
+    "url": "/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "bce769a1123a2eca1ff8",
+    "url": "/static/css/main.9742c61d.chunk.css"
+  },
+  {
+    "revision": "de59c745d6f87175dcba",
+    "url": "/static/js/2.ecf14e7c.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ecf14e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce769a1123a2eca1ff8",
+    "url": "/static/js/main.db3662e6.chunk.js"
+  },
+  {
+    "revision": "4789b4d78fc037fcebac",
+    "url": "/static/js/runtime-main.defac879.js"
+  }
+]);
